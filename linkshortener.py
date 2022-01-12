6a140b1346e77ecd5376dae5cf196640e6b9c81c
Advik Singh Somvanshi <github@advik.dev>
2022-01-12 18:25:27 +0530

    Fixed bugs (or something)

--- linkshortener.py
+++ linkshortener.py
@@ -1,5 +1,7 @@
 """
-Link shortener with 6 shortening services and 1 custom domain service.
+Link shortener with 6 shortening services!
+Integrating 6 total services was to improve efficiency in case if this script stops working because of "Too Many Requests".
+So choosing any random out of 6 seemed kinda reasonable (idk, I tried).
 """
 
 import random, os
@@ -10,8 +12,13 @@ cuttly = os.environ['cuttly']
 
 
 def shortener(link):
+    itdoes = 0
     try:
-        if link.startswith('https://') or link.startswith('http://'):
+        startshere = ["https://", "http://", "www.", "www1."]
+        for tld in startshere:
+            if link.startswith(tld):
+                itdoes = 1
+        if itdoes == 1:
             services = ["bitly", 'cuttly',  'tinyurl', 'dagd', 'isgd' , 'chilpit']
             service = random.choice(services)
             if service == 'bitly':
@@ -41,7 +48,7 @@ def shortener(link):
             else:
                 return "Oops..." #Unnecessary line, but looks good!
         else:
-            return "Invalid Link! Make sure the link starts with https:// or http://"
+            return "Invalid Link! Make sure the link starts with https, http or www"
             
     except ps.exceptions.ShorteningErrorException:
         return "Error Occured! Most likely invalid URL format or server-side error, please try again."
